842646900b6df308dbd103f9ee94cdd768d36dca
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-02-26 09:15:17 +0530

    Added a favicon and title to the website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useMemo, useRef } from "react";
 import "./App.css";
 import { constants, calcDay, calcEvents, calendarFetch } from "./utils";
+import Favicon from "react-favicon";
 
 // Importing all the background images
 import image1 from "./backgrounds/bg1.png";
@@ -52,15 +53,7 @@ function App() {
 
 	return (
 		<div style={{ "--bg": `url(${images[randomImage]})` }} className="mainContainer">
-			<div className="jumpDay" onClick={() => currDay.current?.scrollIntoView({ behavior: "smooth", block: "center" })}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-					/>
-				</svg>
-			</div>
+			<JumpDay currDay={currDay} />
 			<h1 className="Nav">Time: {TimeString}</h1>
 			<h2>
 				Current Events:
@@ -125,6 +118,20 @@ function Day({ day, month, year, active }) {
 						</h2>
 					))}
 			</div>
+		</div>
+	);
+}
+
+function JumpDay({ activeDayRef }) {
+	return (
+		<div className="jumpDay" onClick={() => activeDayRef.current?.scrollIntoView({ behavior: "smooth", block: "center" })}>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+				/>
+			</svg>
 		</div>
 	);
 }
